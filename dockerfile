b9b2321a62f21d0cba6ad3d29d65f3a5f690e545
prk <princessjl123@gmail.com>
2021-02-18 08:03:44 -0800

    add file

---
+++ dockerfile
@@ -0,0 +1,19 @@
+FROM php:7.4-apache
+
+EXPOSE 80
+
+RUN apt-get update
+RUN apt-get install nano
+
+RUN chmod 774 -R /var/www
+RUN chmod 774 -R /var/www/html
+RUN chown -R www-data:www-data /var/www
+RUN chown -R www-data:www-data /var/www/html
+
+COPY index.php /var/www/html
+
+RUN docker-php-ext-install mysqli
+RUN docker-php-ext-enable mysqli
+RUN apachectl restart
+
+CMD ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$link = mysqli_connect("172.19.0.2", "root", "abcde", "trucorp");
+
+$query = "SELECT * FROM users";
+
+$result = $link->query($query);
+
+if(!$link)
+{
+ echo "Not Connected to SQL" . PHP_EOL;
+ exit;
+}
+else
+{
+ foreach($result as $row)
+ {
+   echo "[ID] " . $row["ID"] . "<br>";
+   echo "[Nama] " . $row["Nama"] . "<br>";
+   echo "[Kantor (Pusat/Cabang)] " . $row["Kantor"] . "<br>";
+ }
+}
+
+mysqli_close($link);
+
+?>
